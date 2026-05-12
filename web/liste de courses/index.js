322bb11b9ddb70c183114455bef6a4517d2a037a
Morgan-Saturn <morgan.tunteva@proton.me>
2026-05-12 19:37:15 +0200

    create portfolio website

--- web/liste de courses/index.js
+++ web/liste de courses/index.js
@@ -3,43 +3,25 @@ const nouvItem = document.getElementById("ajout");
 const formulaire = document.querySelector(".upper");
 let liste = [];
 
-plus.addEventListener("click", addItem)
-formulaire.addEventListener("submit", addItem)
+
+get_local_list()
+
+plus.addEventListener("click", handleAddItem)
+formulaire.addEventListener("submit", handleAddItem)
 
 //création de la fonction permettant d'ajouter un nouvel objet à la liste de courses
-function addItem(event){
+function handleAddItem(event){
     //ici on fait en sorte qu'en cliquant sur "entrer" le formulaire ne se reset pas et ajoute le nouvel élément entré
     event.preventDefault()
     if (formulaire.checkValidity() == false){
         return;
     }
     
-    let listitems = document.querySelector(".listitems");
-    let elementId = liste.length;
-    
-    const minus = document.getElementById("decrement");
+    let item = {item:nouvItem.value, count: 1}
+    addItemDOM(item)
 
-    let newItem = document.createElement("div");
-    newItem.className="rest";
-    newItem.innerHTML = `<div class="row">
-        <input type="checkbox" id="check${elementId}"/>
-            <label for="check${elementId}">${nouvItem.value}</label>
-            <div class="compteur">
-                <button class="decrement" onclick="decrease_number(event)"> - </button>
-                <input type="number" min="1" value=1 />
-                <button class="increment" onclick="increase_number(event)"> + </button>
-            </div>
-            <i class="fa-solid fa-trash"></i>
-        </div>`;
-        
-    liste.push({item:nouvItem.value, count: 1});
-    listitems.prepend(newItem);
+    save_list(liste);
 
-    //création de la constante permettant de supprimer l'élément voulu en cliquant sur l'icône poubelle
-    const suppr = document.querySelector(".fa-solid.fa-trash");
-    suppr.addEventListener("click", () => {
-        newItem.remove();
-    });
     //une fois qu'on a rentré le nouvel item, après clic sur "+" ou la touche "entrer", l'input type text se vide de son contenu
     nouvItem.value = "";
 }
@@ -67,4 +49,48 @@ function decrease_number(event){
     } else{
         counter.value = counter.value - 1
     }
+}
+
+function addItemDOM(item) {
+    
+    let listitems = document.querySelector(".listitems");
+    let elementId = liste.length;
+
+    let newItem = document.createElement("div");
+    newItem.className="rest";
+    newItem.innerHTML = `<div class="row">
+        <input type="checkbox" id="check${elementId}"/>
+            <label for="check${elementId}">${item.item}</label>
+            <div class="compteur">
+                <button class="decrement" onclick="decrease_number(event)"> - </button>
+                <input type="number" min="1" value=1 />
+                <button class="increment" onclick="increase_number(event)"> + </button>
+            </div>
+            <i class="fa-solid fa-trash"></i>
+        </div>`;
+
+    liste.push(item);
+    listitems.prepend(newItem);
+
+    //création de la constante permettant de supprimer l'élément voulu en cliquant sur l'icône poubelle
+    const suppr = document.querySelector(".fa-solid.fa-trash");
+    suppr.addEventListener("click", () => {
+        newItem.remove();
+    });
+}
+
+//fonction sauvegardant la liste de courses en local storage
+
+function save_list(liste){
+   
+   localStorage.setItem("shopping_list", JSON.stringify(liste))
+}
+
+//fonction recherchant s'il existe des éléments dans le local storage
+function get_local_list(){
+
+    const saved_list = JSON.parse(localStorage.getItem("shopping_list")) || [];
+    
+    saved_list.forEach(addItemDOM);
+    
 }
